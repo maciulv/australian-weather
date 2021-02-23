cd2f03dd4dfce85e2ed7e9e09f8a02787683f1d4
maciulv <valdonemac@gmail.com>
2021-02-23 16:39:56 +0000

    Add ggplot theme

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,13 +9,15 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # a location
 
-city = "Sydney"
+city = "Brisbane"
 
-scatter_plot = 
-df %>%
-  filter(Location == city) %>%
-  ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour= "red" )
+scatter_plot =
+  df %>%
+  filter(Location == "Brisbane") %>%
+  ggplot(aes(x = Temp9am, y = Humidity9am)) +
+  geom_point(colour = "red") +
+  labs(title = "Humidity and Temperatur for Brisbane") +
+  theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
