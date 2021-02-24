5626a4642250259246a65064ca66dccdf59cd778
maciulv <valdonemac@gmail.com>
2021-02-24 15:09:10 +0000

    Time_series added

--- R/create-plot.R
+++ R/create-plot.R
@@ -19,5 +19,12 @@ scatter_plot =
   labs(title = "Humidity and Temperatur for Brisbane") +
   theme_minimal()
 
+time_series =
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x = Date, y = Temp9am)) +
+  geom_line()
+ggsave(filename = "graphics/timeseries-rainfall.png",
+       plot = scatter_plot)
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
